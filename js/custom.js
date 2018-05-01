2484f86889de320cca441a94c4bf3412124bcd2b
justinwaves <juhstinn@gmail.com>
2018-05-01 00:20:38 -0700

    Accordian + more
    I created an accordion from scratch without using any JS libraries. I
    also used JS to change the direction of my arrow buttons, however I can
    not get it to toggle back and I refuse to cheat. Will hope back on it
    tomorrow and try and figure out whats wrong with my code. =/
    
    I also added a contact tab <3

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,36 @@
-function main(){
-$('.title').hide();
-$('.title').fadeIn(1000);
-}
 
-$(document).ready(main);
+
+
+var buttons = document.querySelectorAll(".menuContainer");
+var arrows = document.querySelectorAll(".menuButtons");
+
+
+
+for (i = 0; i < buttons.length; i ++) {
+  buttons[i].onclick = function() {
+    
+    var content = this.nextElementSibling;
+    var arrows = document.querySelectorAll(".menuButtons");
+      
+    if(content.style.maxHeight){
+      content.style.maxHeight = null;  
+        
+    } else{
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+};
+
+for (i = 0; i < arrows.length; i ++) {
+  arrows[i].onclick = function() {
+    
+      
+    if (arrows.innerHTML="<i class='fas fa-angle-left' id='leftIcon'></i>") {
+         arrows = this.innerHTML ="<i class='fas fa-angle-down' id='leftIcon'></i>"; 
+     
+    } else {
+    arrows= this.innerHTML ="<i class='fas fa-angle-left' id='leftIcon'></i>"; 
+    }
+  }
+};
+
